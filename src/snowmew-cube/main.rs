113f0f27efe238d7b6e38ebc49d81d8ddc55ea48
Colin Sherratt <colin.sherratt@gmail.com>
2013-11-18 01:42:37 -0500

    fixed faces

--- src/snowmew-cube/main.rs
+++ src/snowmew-cube/main.rs
@@ -13,55 +13,56 @@ static VS_SRC: &'static str =
 "#version 150\n\
 uniform mat4 mat; \n\
 in vec3 position;\n\
-out vec2 UV;\n\
+out vec3 UV;\n\
 void main() {\n\
     gl_Position = mat * vec4(position, 1.);\n\
-    UV = vec2(position.x, position.y); \n\
+    UV = vec3(position.x, position.y, position.z); \n\
 }";
 
 static FS_SRC: &'static str =
 "#version 150\n\
 out vec4 out_color;\n\
-in vec2 UV;\n \
+in vec3 UV;\n \
 void main() {\n\
-    gl_FragColor = vec4(UV.x, UV.y, 1, 0);\n\
+    gl_FragColor = vec4(UV.x, UV.y, UV.z, 0);\n\
 }";
 
 static VERTEX_DATA: [f32, ..24] = [
-    -1., -1., -1.,
-    -1.,  1., -1.,
-     1., -1., -1.,
-     1.,  1., -1.,
     -1., -1.,  1.,
     -1.,  1.,  1.,
      1., -1.,  1.,
      1.,  1.,  1.,
+    -1., -1., -1.,
+    -1.,  1., -1.,
+     1., -1., -1.,
+     1.,  1., -1.,
 ];
 
 static INDEX_DATA: [u16, ..36] = [
     // top
-    0, 1, 2,
-    1, 2, 3,
+    0, 2, 1,
+    2, 3, 1,
+
 
     // bottom
-    4, 5, 6,
-    5, 6, 7,
+    5, 7, 4,
+    7, 6, 4,
 
     // right
     1, 3, 5,
-    3, 5, 7,
+    3, 7, 5,
 
     // left
-    0, 2, 4,
-    2, 4, 6,
+    4, 6, 0,
+    6, 2, 0,
 
     // front
-    2, 3, 6,
-    3, 6, 7,
+    4, 0, 5,
+    0, 1, 5,
 
     // back
-    0, 1, 4,
-    1, 4, 5
+    2, 6, 3,
+    6, 7, 3,
 ];
 
 
@@ -101,13 +102,15 @@ fn main() {
 
         gl::Enable(gl::SCISSOR_TEST);
         gl::Enable(gl::DEPTH_TEST);
+        gl::Enable(gl::CULL_FACE);
+        gl::CullFace(gl::BACK);
 
         let mut rot = 0f32;
 
         while !window.should_close() {
             glfw::poll_events();
 
-            let transform = cgmath::matrix::Mat4::scale(0.5f32, 0.5f32, 0.5f32);
+            let transform = cgmath::matrix::Mat4::scale(1f32, 1f32, 1f32);
             let transform = cgmath::matrix::Mat3::from_angle_x(cgmath::angle::rad(rot)).to_mat4().mul_m(&transform);
             let transform = cgmath::matrix::Mat3::from_angle_y(cgmath::angle::rad(rot)).to_mat4().mul_m(&transform);
             let transform = cgmath::matrix::Mat3::from_angle_z(cgmath::angle::rad(rot)).to_mat4().mul_m(&transform);
@@ -120,7 +123,7 @@ fn main() {
                 dt.draw(&shader, &geo, &[(&"mat", &transform as &snowmew::coregl::Uniforms)], &[]);             
             }
             window.swap_buffers();
-            rot += 0.01f32;
+            rot += 0.005f32;
         }
     }
 
