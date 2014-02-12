3037fb0bf80f9f252824c35d1568b376e78c1c9c
Colin Sherratt <colin.sherratt@gmail.com>
2014-02-12 01:49:37 -0500

    Added timing

---
+++ src/snowmew/timing.rs
@@ -0,0 +1,43 @@
+
+use extra::time::precise_time_s;
+
+#[deriving(Clone)]
+pub struct Timing
+{
+    priv time: ~[(f64, ~str)]
+}
+
+impl Timing
+{
+    pub fn new() -> Timing
+    {
+        Timing {
+            time: ~[]
+        }
+    }
+
+    pub fn reset(&mut self)
+    {
+        self.time = ~[(precise_time_s(), ~"start")]
+    }
+
+    pub fn mark(&mut self, name: ~str)
+    {
+        self.time.push((precise_time_s(), name))
+    }
+
+    pub fn dump(&self)
+    {
+        if self.time.len() == 0 {
+            println!("no timing");
+        } else {
+            let (mut last, _) = self.time[0];
+
+            for &(time, ref name) in self.time.iter() {
+                print!("=={:2.1f}ms==> {:s} ", 1000. * (time - last), *name);
+                last = time;
+            }
+            println!("");
+        }
+    }
+}
