6495e77fa421ea187ffc60e508412bd2508a7fb0
Colin Sherratt <colin.sherratt@gmail.com>
2014-05-20 13:20:19 -0400

    added flip

--- src/snowmew-graphics/texture.rs
+++ src/snowmew-graphics/texture.rs
@@ -8,6 +8,29 @@ pub struct Texture {
     data: Vec<u8>
 }
 
+fn offset(width: uint, depth: uint,
+          row: uint, column: uint, component: uint) -> uint {
+    width * depth * row +
+    depth * column +
+    component
+}
+
+fn flip(dat: &mut Vec<u8>, height: uint, width: uint, depth: uint) {
+    for row in range(0, height/2) {
+        let swap_row = height - row - 1;
+        for column in range(0, width) {
+            for d in range(0, depth) {
+                let a_addr = offset(width, depth, row, column, d);
+                let b_addr = offset(width, depth, swap_row, column, d);
+                let temp = *dat.get(a_addr);
+                *dat.get_mut(a_addr) = *dat.get(b_addr);
+                *dat.get_mut(b_addr) = temp;
+            }
+        }
+    }
+}
+
+
 impl default::Default for Texture {
     fn default() -> Texture {
         Texture::new(0, 0, 0, Vec::new())
@@ -28,4 +51,7 @@ impl Texture {
     pub fn height(&self) -> uint { self.height }
     pub fn depth(&self) -> uint { self.depth }
     pub fn data<'a>(&'a self) -> &'a [u8] { self.data.as_slice() }
+    pub fn flip(&mut self) {
+        flip(&mut self.data, self.height, self.width, self.depth);
+    }
 }
