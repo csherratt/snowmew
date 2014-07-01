b00ed6aff03c8dcde632f273aae9b55bea9f65d6
Colin Sherratt <colin.sherratt@gmail.com>
2014-07-01 14:06:36 -0400

    added vertex loading

--- src/snowmew-render-gfx/lib.rs
+++ src/snowmew-render-gfx/lib.rs
@@ -20,6 +20,7 @@ use std::comm::{Receiver, Sender};
 use std::mem;
 use std::sync::TaskPool;
 use std::sync::Future;
+use std::collections::hashmap::HashMap;
 
 use OpenCL::hl::{CommandQueue, Context, Device};
 use sync::Arc;
@@ -29,11 +30,46 @@ use graphics::Graphics;
 use snowmew::common::ObjectKey;
 use snowmew::io::Window;
 
+use graphics::geometry::{Vertex, VertexGeo, VertexGeoTex, VertexGeoNorm, VertexGeoTexNorm, VertexGeoTexNormTan};
+use graphics::geometry::{Geo, GeoTex, GeoNorm, GeoTexNorm, GeoTexNormTan};
+
+
+static VERTEX_SRC: &'static [u8] = b"
+    #version 150 core
+    in vec2 a_Pos;
+    out vec4 v_Color;
+    void main() {
+        v_Color = vec4(a_Pos+0.5, 0.0, 1.0);
+        gl_Position = vec4(a_Pos, 0.0, 1.0);
+    }
+";
+
+static FRAGMENT_SRC: &'static [u8] = b"
+    #version 150 core
+    in vec4 v_Color;
+    out vec4 o_Color;
+    uniform sampler3D tex3D;
+    uniform MyBlock {
+        vec4 color;
+    } block;
+    void main() {
+        vec4 texel = texture(tex3D, vec3(0.5,0.5,0.5));
+        vec4 unused = mix(texel, block.color, 0.5);
+        o_Color = v_Color.x<0.0 ? unused : v_Color;
+    }
+";
+
 pub trait RenderData : Graphics + Positions {}
 
+struct Mesh {
+    mesh: uint,
+    index: u32
+}
+
 pub struct RenderManager {
-    server: gfx::Device<snowmew::io::Window>,
-    client: gfx::Renderer
+    client: gfx::Renderer,
+    program: Option<uint>,
+    meshes: HashMap<ObjectKey, Mesh>
 }
 
 impl RenderManager {
@@ -42,18 +78,91 @@ impl RenderManager {
             _: (i32, i32),
             _: Option<Arc<Device>>) -> RenderManager {
 
+        glfw::make_context_current(None);
+        spawn(proc() {
+            let mut server = server;
+            server.make_current();
+            loop { server.update(); }
+        });
+
         RenderManager {
-            server: server,
-            client: client
+            client: client,
+            program: None,
+            meshes: HashMap::new()
+        }
+    }
+
+    fn load<RD: RenderData>(&mut self, db: &RD) {
+        if self.program.is_none() {
+            self.program = Some(self.client.create_program(VERTEX_SRC.to_owned(),
+                                                           FRAGMENT_SRC.to_owned()))
         }
 
+        for (oid, vb) in db.vertex_buffer_iter() {
+            if self.meshes.find(oid).is_none() {
+                let mut data: Vec<f32> = Vec::new();
+                match vb.vertex {
+                    Geo(ref d) => {
+                        for v in d.iter() {
+                            data.push(v.position.x);
+                            data.push(v.position.y);
+                            data.push(v.position.z);
+                        }
+                    },
+                    GeoTex(ref d) => {
+                        for v in d.iter() {
+                            data.push(v.position.x);
+                            data.push(v.position.y);
+                            data.push(v.position.z);
+                        }
+                    },
+                    GeoNorm(ref d) => {
+                        for v in d.iter() {
+                            data.push(v.position.x);
+                            data.push(v.position.y);
+                            data.push(v.position.z);
+                        }
+                    },
+                    GeoTexNorm(ref d) => {
+                        for v in d.iter() {
+                            data.push(v.position.x);
+                            data.push(v.position.y);
+                            data.push(v.position.z);
+                        }
+                    },
+                    GeoTexNormTan(ref d) => {
+                        for v in d.iter() {
+                            data.push(v.position.x);
+                            data.push(v.position.y);
+                            data.push(v.position.z);
+                        }
+                    }
+                }
+                let mesh = self.client.create_mesh((data.len() / 3) as u16,
+                                                   data,
+                                                   3,
+                                                   12);
+
+                let mut index = Vec::new();
+                for &i in vb.index.iter() {
+                    index.push(i as u16);
+                }
+
+                let index = self.client.create_index_buffer(index);
+
+                self.meshes.insert(*oid, Mesh {
+                    index: index,
+                    mesh: mesh
+                });
+            }
+        }
     }
 }
 
 
 impl<RD: RenderData+Send> snowmew::Render<RD> for RenderManager {
     fn update(&mut self, db: RD, scene: ObjectKey, camera: ObjectKey) {
-        fail!("not supported")
+        self.load(&db);
     }
 }
 
