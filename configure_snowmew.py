06136ab7cecbe1d93c040bfbc9159a1f786dde21
Colin Sherratt <colin.sherratt@gmail.com>
2014-07-03 01:09:09 -0400

    added gl dependency to device

--- configure_snowmew.py
+++ configure_snowmew.py
@@ -315,7 +315,7 @@ modules = [Bin("demo-noclip", ["snowmew", "snowmew-render-mux", "snowmew-loader"
            Lib("collision", ["cgmath"]),
            Lib("OpenCL"),
            Lib("gfx", ["glfw", "gl", "device"], other_flags="--cfg=gl --cfg=glfw"),
-           Lib("device", other_flags="--cfg=gl --cfg=glfw"),
+           Lib("device", ["gl"], other_flags="--cfg=gl --cfg=glfw"),
            Lib("stb-image", ["libstb-image.a"]),
            LibConfigureMakefile("libstb-image.a", "modules/stb-image/", ["modules/stb-image/libstb-image.a"]),
            LibCMake("libglfw3.a", "modules/glfw/", ["modules/glfw/src/libglfw3.a"], cmake_flags="-DCMAKE_C_FLAGS=\"-fPIC\""),
