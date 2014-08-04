2c6dffe6793a36c9f2049ab2bdc99252b68edd31
Colin Sherratt <colin.sherratt@gmail.com>
2014-08-04 15:35:33 -0400

    Added snowmew physics

--- src/snowmew.rs
+++ src/snowmew.rs
@@ -2,3 +2,12 @@
 
 extern crate core = "snowmew-core";
 extern crate graphics = "snowmew-graphics";
+extern crate loader = "snowmew-loader";
+extern crate physics = "snowmew-physics";
+extern crate position = "snowmew-position";
+extern crate render = "snowmew-render-mux";
+
+pub mod config  {
+    pub use SnowmewConfig = core::SnowmewConfig;
+}
+
