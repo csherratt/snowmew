9d1c387d9936feac27cdeaa01fde6a46aee2b905
Colin Sherratt <colin.sherratt@gmail.com>
2014-08-06 03:43:22 -0400

    Added working gfx texturing

--- src/snowmew-render-gfx/lib.rs
+++ src/snowmew-render-gfx/lib.rs
@@ -1,4 +1,4 @@
-//   Copyright 2014 Colin Sherratt
+    //   Copyright 2014 Colin Sherratt
 //
 //   Licensed under the Apache License, Version 2.0 (the "License");
 //   you may not use this file except in compliance with the License.
@@ -63,9 +63,13 @@ GLSL_150: b"
     uniform mat4 model_mat;
 
     in vec3 position;
+    in vec2 texture;
+
+    out vec2 o_texture;
 
     void main() {
         gl_Position = proj_mat * view_mat * model_mat * vec4(position, 1.0);
+        o_texture = texture;
     }
 "
 };
@@ -73,10 +77,20 @@ GLSL_150: b"
 static FRAGMENT_SRC: gfx::ShaderSource = shaders! {
 GLSL_150: b"
     #version 150 core
+    uniform vec4 ka_color;
+    uniform int ka_use_texture;
+    uniform sampler2D ka_texture;
+
+    in vec2 o_texture;
+
     out vec4 o_Color;
-    uniform vec4 color;
+
     void main() {
-        o_Color = color;
+        if (1 == ka_use_texture) {
+            o_Color = texture(ka_texture, o_texture);
+        } else {
+            o_Color = ka_color;
+        }
     }
 "
 };
@@ -86,7 +100,9 @@ struct Params {
     proj_mat: [[f32, ..4], ..4],
     view_mat: [[f32, ..4], ..4],
     model_mat: [[f32, ..4], ..4],
-    color: [f32, ..4]
+    ka_color: [f32, ..4],
+    ka_use_texture: i32,
+    ka_texture: gfx::shade::TextureParam,
 }
 
 struct Mesh {
@@ -99,7 +115,9 @@ pub struct RenderManager {
     frame: render::target::Frame,
     state: render::state::DrawState,
     prog: render::shade::CustomShell<_ParamsLink,Params>,
-    meshes: HashMap<ObjectKey, Mesh>
+    meshes: HashMap<ObjectKey, Mesh>,
+    textures: HashMap<ObjectKey, render::TextureHandle>,
+    sampler: render::SamplerHandle
 }
 
 impl RenderManager {
@@ -117,7 +135,7 @@ impl RenderManager {
             loop { 
                 server.update();
                 let end = time::precise_time_s();
-                println!("{:4.1}fps", 1. / (end - start));
+                //println!("{:4.1}fps", 1. / (end - start));
                 start = end;
             }
         });
@@ -126,7 +144,26 @@ impl RenderManager {
         let frame =  gfx::Frame::new(width as u16, height as u16);
         let state = gfx::DrawState::new().depth(gfx::state::LessEqual, true);
 
+        let sampler = client.create_sampler(
+            gfx::tex::SamplerInfo::new(
+                    gfx::tex::Bilinear, gfx::tex::Tile
+            )
+        );
+
         let prog = {
+            let tinfo = gfx::tex::TextureInfo {
+                width: 1,
+                height: 1,
+                depth: 1,
+                mipmap_range: (0, 1),
+                kind: gfx::tex::Texture2D,
+                format: gfx::tex::RGBA8,
+            };
+
+            let img_info = tinfo.to_image_info();
+            let dummy_texture = client.create_texture(tinfo);
+            client.update_texture(dummy_texture, img_info, vec![0u8, 0, 0, 0]);
+
             let data = Params {
                 proj_mat: [
                     [1.0, 0.0, 0.0, 0.0],
@@ -146,7 +183,9 @@ impl RenderManager {
                     [0.0, 0.0, 1.0, 0.0],
                     [0.0, 0.0, 0.0, 1.0],
                 ],
-                color: [1., 1., 1., 1.]
+                ka_use_texture: 0,
+                ka_color: [1., 1., 1., 1.],
+                ka_texture: (dummy_texture, Some(sampler))
             };
             let handle = client.create_program(VERTEX_SRC.clone(), FRAGMENT_SRC.clone());
             client.connect_program(handle, data).unwrap()
@@ -157,11 +196,13 @@ impl RenderManager {
             frame: frame,
             state: state,
             prog: prog,
-            meshes: HashMap::new()
+            meshes: HashMap::new(),
+            textures: HashMap::new(),
+            sampler: sampler
         }
     }
 
-    fn load<RD: RenderData>(&mut self, db: &RD) {
+    fn load_meshes<RD: RenderData>(&mut self, db: &RD) {
         for (oid, vb) in db.vertex_buffer_iter() {
             if self.meshes.find(oid).is_none() {
                 let mesh = match vb.vertex {
@@ -182,7 +223,7 @@ impl RenderManager {
                     }
                 };
 
-                let vb: Vec<u16> = vb.index.iter().map(|&x| x as u16).collect();
+                let vb: Vec<u32> = vb.index.iter().map(|&x| x as u32).collect();
 
                 let index = self.client.create_buffer(Some(vb));
 
@@ -190,6 +231,30 @@ impl RenderManager {
                     index: index,
                     mesh: mesh
                 });
+            }
+        }
+    }
+
+    fn load_textures<RD: RenderData>(&mut self, db: &RD) {
+        for (oid, text) in db.texture_iter() {
+            if self.textures.find(oid).is_none() {
+                let tinfo = gfx::tex::TextureInfo {
+                    width: text.width() as u16,
+                    height: text.height() as u16,
+                    depth: 1 as u16,
+                    mipmap_range: (0, 1),
+                    kind: gfx::tex::Texture2D,
+                    format: match text.depth() {
+                        4 => gfx::tex::RGBA8,
+                        3 => gfx::tex::RGB8,
+                        _ => fail!("Unsupported color depth")
+                    }
+                };
+
+                let img_info = tinfo.to_image_info();
+                let texture = self.client.create_texture(tinfo);
+                self.client.update_texture(texture, img_info, text.data().to_vec());
+                self.textures.insert(*oid, texture);
             }
         }
     }
@@ -236,15 +301,26 @@ impl RenderManager {
                  [model.z.x, model.z.y, model.z.z, model.z.w],
                  [model.w.x, model.w.y, model.w.z, model.w.w]];
 
-            let [r, g, b] = mat.ka();
-            self.prog.data.color = [r, g, b, 1.];
 
-            self.client.draw(&vb.mesh, 
-                             gfx::IndexSlice(vb.index, geo.offset as u32, geo.count as u32),
-                             self.frame,
-                             &self.prog,
-                             self.state
-            ).ok().expect("Failed to render");
+            self.prog.data.ka_use_texture = match mat.map_ka() {
+                Some(tid) => {
+                    let &texture = self.textures.find(&tid).expect("Texture not loaded");
+                    self.prog.data.ka_texture = (texture, Some(self.sampler));
+                    1
+                }
+                None => {
+                    let [r, g, b] = mat.ka();
+                    self.prog.data.ka_color = [r, g, b, 1.];
+                    0
+                }
+            };
+
+            let _ = self.client.draw(&vb.mesh, 
+                 gfx::IndexSlice(vb.index, geo.offset as u32, geo.count as u32),
+                 &self.frame,
+                 &self.prog,
+                 &self.state
+            );
         }
 
         self.client.end_frame();
@@ -254,7 +330,8 @@ impl RenderManager {
 
 impl<RD: RenderData+Send> snowmew::Render<RD> for RenderManager {
     fn update(&mut self, db: RD, scene: ObjectKey, camera: ObjectKey) {
-        self.load(&db);
+        self.load_meshes(&db);
+        self.load_textures(&db);
         self.draw(&db, scene, camera);
     }
 }
