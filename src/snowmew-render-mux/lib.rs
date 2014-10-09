f3417d06f0b3774bc20bafed15389c440a799a8b
Colin Sherratt <colin.sherratt@gmail.com>
2014-10-08 22:51:35 -0400

    Use GFX as defualt render

--- src/snowmew-render-mux/lib.rs
+++ src/snowmew-render-mux/lib.rs
@@ -55,13 +55,13 @@ impl<'r, RD: RenderData+Send> snowmew::RenderFactory<RD, RenderMux<'r, RD>> for 
             size: (i32, i32),
             cl: Option<Arc<Device>>) -> RenderMux<RD> {
 
-        let s = match os::getenv("GFX") {
+        let s = match os::getenv("AZDO") {
             Some(s) => s,
             None => "false".to_string()
         };
         let s: String = s.as_slice().chars().map(|c| c.to_lowercase()).collect();
 
-        let use_gfx = match s.as_slice() {
+        let use_azdo = match s.as_slice() {
             "true" => Some(true),
             "enabled" => Some(true),
             "1" => Some(true),
@@ -71,15 +71,15 @@ impl<'r, RD: RenderData+Send> snowmew::RenderFactory<RD, RenderMux<'r, RD>> for 
             _ => None
         };
 
-        let rm: RenderMux<RD> = if use_gfx.is_some() && use_gfx.unwrap() {
-            let rf: Box<snowmew::RenderFactory<RD, gfx::RenderManager<RD>>> = box gfx::RenderFactory::new();
-            let render: Box<gfx::RenderManager<RD>> = box rf.init(io, window, size, cl);
+        let rm: RenderMux<RD> = if use_azdo.is_some() && use_azdo.unwrap() {
+            let rf: Box<snowmew::RenderFactory<RD, azdo::RenderManager>> = box azdo::RenderFactory::new();
+            let render: Box<azdo::RenderManager> = box rf.init(io, window, size, cl);
             RenderMux {
                 render: render as Box<snowmew::Render<RD>>
             }
         } else {
-            let rf: Box<snowmew::RenderFactory<RD, azdo::RenderManager>> = box azdo::RenderFactory::new();
-            let render: Box<azdo::RenderManager> = box rf.init(io, window, size, cl);
+            let rf: Box<snowmew::RenderFactory<RD, gfx::RenderManager<RD>>> = box gfx::RenderFactory::new();
+            let render: Box<gfx::RenderManager<RD>> = box rf.init(io, window, size, cl);
             RenderMux {
                 render: render as Box<snowmew::Render<RD>>
             }
