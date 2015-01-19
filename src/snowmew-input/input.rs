79f23b6e22b36e990d5b662dba27624259223fe0
Colin Sherratt <colin.sherratt@gmail.com>
2015-01-19 01:30:20 -0500

    start moving logic to snowmew-input

---
+++ src/snowmew-input/input.rs
@@ -0,0 +1,346 @@
+//   Copyright 2014 Colin Sherratt
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+use glfw;
+
+#[derive(Clone, Show, Eq, PartialEq, Hash, RustcDecodable, RustcEncodable, Copy)]
+pub enum Button {
+    KeyboardSpace,
+    KeyboardApostrophe,
+    KeyboardComma,
+    KeyboardMinus,
+    KeyboardPeriod,
+    KeyboardSlash,
+    Keyboard0,
+    Keyboard1,
+    Keyboard2,
+    Keyboard3,
+    Keyboard4,
+    Keyboard5,
+    Keyboard6,
+    Keyboard7,
+    Keyboard8,
+    Keyboard9,
+    KeyboardSemicolon,
+    KeyboardEqual,
+    KeyboardA,
+    KeyboardB,
+    KeyboardC,
+    KeyboardD,
+    KeyboardE,
+    KeyboardF,
+    KeyboardG,
+    KeyboardH,
+    KeyboardI,
+    KeyboardJ,
+    KeyboardK,
+    KeyboardL,
+    KeyboardM,
+    KeyboardN,
+    KeyboardO,
+    KeyboardP,
+    KeyboardQ,
+    KeyboardR,
+    KeyboardS,
+    KeyboardT,
+    KeyboardU,
+    KeyboardV,
+    KeyboardW,
+    KeyboardX,
+    KeyboardY,
+    KeyboardZ,
+    KeyboardLeftBracket,
+    KeyboardBackslash,
+    KeyboardRightBracket,
+    KeyboardGraveAccent,
+    KeyboardWorld1,
+    KeyboardWorld2,
+    KeyboardEscape,
+    KeyboardEnter,
+    KeyboardTab,
+    KeyboardBackspace,
+    KeyboardInsert,
+    KeyboardDelete,
+    KeyboardRight,
+    KeyboardLeft,
+    KeyboardDown,
+    KeyboardUp,
+    KeyboardPageUp,
+    KeyboardPageDown,
+    KeyboardHome,
+    KeyboardEnd,
+    KeyboardCapsLock,
+    KeyboardScrollLock,
+    KeyboardNumLock,
+    KeyboardPrintScreen,
+    KeyboardPause,
+    KeyboardF1,
+    KeyboardF2,
+    KeyboardF3,
+    KeyboardF4,
+    KeyboardF5,
+    KeyboardF6,
+    KeyboardF7,
+    KeyboardF8,
+    KeyboardF9,
+    KeyboardF10,
+    KeyboardF11,
+    KeyboardF12,
+    KeyboardF13,
+    KeyboardF14,
+    KeyboardF15,
+    KeyboardF16,
+    KeyboardF17,
+    KeyboardF18,
+    KeyboardF19,
+    KeyboardF20,
+    KeyboardF21,
+    KeyboardF22,
+    KeyboardF23,
+    KeyboardF24,
+    KeyboardF25,
+    KeyboardKp0,
+    KeyboardKp1,
+    KeyboardKp2,
+    KeyboardKp3,
+    KeyboardKp4,
+    KeyboardKp5,
+    KeyboardKp6,
+    KeyboardKp7,
+    KeyboardKp8,
+    KeyboardKp9,
+    KeyboardKpDecimal,
+    KeyboardKpDivide,
+    KeyboardKpMultiply,
+    KeyboardKpSubtract,
+    KeyboardKpAdd,
+    KeyboardKpEnter,
+    KeyboardKpEqual,
+    KeyboardLeftShift,
+    KeyboardLeftControl,
+    KeyboardLeftAlt,
+    KeyboardLeftSuper,
+    KeyboardRightShift,
+    KeyboardRightControl,
+    KeyboardRightAlt,
+    KeyboardRightSuper,
+    KeyboardMenu,
+    MouseLeft,
+    MouseRight,
+    MouseCenter,
+    MouseExt0,
+    MouseExt1,
+    MouseExt2,
+    MouseExt3,
+    MouseExt4
+}
+
+fn from_glfw_key(key: glfw::Key) -> Button {
+    return match key {
+        glfw::Key::Space => Button::KeyboardSpace,
+        glfw::Key::Apostrophe => Button::KeyboardApostrophe,
+        glfw::Key::Comma => Button::KeyboardComma,
+        glfw::Key::Minus => Button::KeyboardMinus,
+        glfw::Key::Period => Button::KeyboardPeriod,
+        glfw::Key::Slash => Button::KeyboardSlash,
+        glfw::Key::Num0 => Button::Keyboard0,
+        glfw::Key::Num1 => Button::Keyboard1,
+        glfw::Key::Num2 => Button::Keyboard2,
+        glfw::Key::Num3 => Button::Keyboard3,
+        glfw::Key::Num4 => Button::Keyboard4,
+        glfw::Key::Num5 => Button::Keyboard5,
+        glfw::Key::Num6 => Button::Keyboard6,
+        glfw::Key::Num7 => Button::Keyboard7,
+        glfw::Key::Num8 => Button::Keyboard8,
+        glfw::Key::Num9 => Button::Keyboard9,
+        glfw::Key::Semicolon => Button::KeyboardSemicolon,
+        glfw::Key::Equal => Button::KeyboardEqual,
+        glfw::Key::A => Button::KeyboardA,
+        glfw::Key::B => Button::KeyboardB,
+        glfw::Key::C => Button::KeyboardC,
+        glfw::Key::D => Button::KeyboardD,
+        glfw::Key::E => Button::KeyboardE,
+        glfw::Key::F => Button::KeyboardF,
+        glfw::Key::G => Button::KeyboardG,
+        glfw::Key::H => Button::KeyboardH,
+        glfw::Key::I => Button::KeyboardI,
+        glfw::Key::J => Button::KeyboardJ,
+        glfw::Key::K => Button::KeyboardK,
+        glfw::Key::L => Button::KeyboardL,
+        glfw::Key::M => Button::KeyboardM,
+        glfw::Key::N => Button::KeyboardN,
+        glfw::Key::O => Button::KeyboardO,
+        glfw::Key::P => Button::KeyboardP,
+        glfw::Key::Q => Button::KeyboardQ,
+        glfw::Key::R => Button::KeyboardR,
+        glfw::Key::S => Button::KeyboardS,
+        glfw::Key::T => Button::KeyboardT,
+        glfw::Key::U => Button::KeyboardU,
+        glfw::Key::V => Button::KeyboardV,
+        glfw::Key::W => Button::KeyboardW,
+        glfw::Key::X => Button::KeyboardX,
+        glfw::Key::Y => Button::KeyboardY,
+        glfw::Key::Z => Button::KeyboardZ,
+        glfw::Key::LeftBracket => Button::KeyboardLeftBracket,
+        glfw::Key::Backslash => Button::KeyboardBackslash,
+        glfw::Key::RightBracket => Button::KeyboardRightBracket,
+        glfw::Key::GraveAccent => Button::KeyboardGraveAccent,
+        glfw::Key::World1 => Button::KeyboardWorld1,
+        glfw::Key::World2 => Button::KeyboardWorld2,
+        glfw::Key::Escape => Button::KeyboardEscape,
+        glfw::Key::Enter => Button::KeyboardEnter,
+        glfw::Key::Tab => Button::KeyboardTab,
+        glfw::Key::Backspace => Button::KeyboardBackspace,
+        glfw::Key::Insert => Button::KeyboardInsert,
+        glfw::Key::Delete => Button::KeyboardDelete,
+        glfw::Key::Right => Button::KeyboardRight,
+        glfw::Key::Left => Button::KeyboardLeft,
+        glfw::Key::Down => Button::KeyboardDown,
+        glfw::Key::Up => Button::KeyboardUp,
+        glfw::Key::PageUp => Button::KeyboardPageUp,
+        glfw::Key::PageDown => Button::KeyboardPageDown,
+        glfw::Key::Home => Button::KeyboardHome,
+        glfw::Key::End => Button::KeyboardEnd,
+        glfw::Key::CapsLock => Button::KeyboardCapsLock,
+        glfw::Key::ScrollLock => Button::KeyboardScrollLock,
+        glfw::Key::NumLock => Button::KeyboardNumLock,
+        glfw::Key::PrintScreen => Button::KeyboardPrintScreen,
+        glfw::Key::Pause => Button::KeyboardPause,
+        glfw::Key::F1 => Button::KeyboardF1,
+        glfw::Key::F2 => Button::KeyboardF2,
+        glfw::Key::F3 => Button::KeyboardF3,
+        glfw::Key::F4 => Button::KeyboardF4,
+        glfw::Key::F5 => Button::KeyboardF5,
+        glfw::Key::F6 => Button::KeyboardF6,
+        glfw::Key::F7 => Button::KeyboardF7,
+        glfw::Key::F8 => Button::KeyboardF8,
+        glfw::Key::F9 => Button::KeyboardF9,
+        glfw::Key::F10 => Button::KeyboardF10,
+        glfw::Key::F11 => Button::KeyboardF11,
+        glfw::Key::F12 => Button::KeyboardF12,
+        glfw::Key::F13 => Button::KeyboardF13,
+        glfw::Key::F14 => Button::KeyboardF14,
+        glfw::Key::F15 => Button::KeyboardF15,
+        glfw::Key::F16 => Button::KeyboardF16,
+        glfw::Key::F17 => Button::KeyboardF17,
+        glfw::Key::F18 => Button::KeyboardF18,
+        glfw::Key::F19 => Button::KeyboardF19,
+        glfw::Key::F20 => Button::KeyboardF20,
+        glfw::Key::F21 => Button::KeyboardF21,
+        glfw::Key::F22 => Button::KeyboardF22,
+        glfw::Key::F23 => Button::KeyboardF23,
+        glfw::Key::F24 => Button::KeyboardF24,
+        glfw::Key::F25 => Button::KeyboardF25,
+        glfw::Key::Kp0 => Button::KeyboardKp0,
+        glfw::Key::Kp1 => Button::KeyboardKp1,
+        glfw::Key::Kp2 => Button::KeyboardKp2,
+        glfw::Key::Kp3 => Button::KeyboardKp3,
+        glfw::Key::Kp4 => Button::KeyboardKp4,
+        glfw::Key::Kp5 => Button::KeyboardKp5,
+        glfw::Key::Kp6 => Button::KeyboardKp6,
+        glfw::Key::Kp7 => Button::KeyboardKp7,
+        glfw::Key::Kp8 => Button::KeyboardKp8,
+        glfw::Key::Kp9 => Button::KeyboardKp9,
+        glfw::Key::KpDecimal => Button::KeyboardKpDecimal,
+        glfw::Key::KpDivide => Button::KeyboardKpDivide,
+        glfw::Key::KpMultiply => Button::KeyboardKpMultiply,
+        glfw::Key::KpSubtract => Button::KeyboardKpSubtract,
+        glfw::Key::KpAdd => Button::KeyboardKpAdd,
+        glfw::Key::KpEnter => Button::KeyboardKpEnter,
+        glfw::Key::KpEqual => Button::KeyboardKpEqual,
+        glfw::Key::LeftShift => Button::KeyboardLeftShift,
+        glfw::Key::LeftControl => Button::KeyboardLeftControl,
+        glfw::Key::LeftAlt => Button::KeyboardLeftAlt,
+        glfw::Key::LeftSuper => Button::KeyboardLeftSuper,
+        glfw::Key::RightShift => Button::KeyboardRightShift,
+        glfw::Key::RightControl => Button::KeyboardRightControl,
+        glfw::Key::RightAlt => Button::KeyboardRightAlt,
+        glfw::Key::RightSuper => Button::KeyboardRightSuper,
+        glfw::Key::Menu => Button::KeyboardMenu,
+    };
+}
+
+pub fn from_glfw_mouse_button(key: glfw::MouseButton) -> Button {
+    return match key {
+        glfw::MouseButton::Button1 => Button::MouseLeft,
+        glfw::MouseButton::Button2 => Button::MouseRight,
+        glfw::MouseButton::Button3 => Button::MouseCenter,
+        glfw::MouseButton::Button4 => Button::MouseExt0,
+        glfw::MouseButton::Button5 => Button::MouseExt1,
+        glfw::MouseButton::Button6 => Button::MouseExt2,
+        glfw::MouseButton::Button7 => Button::MouseExt3,
+        glfw::MouseButton::Button8 => Button::MouseExt4,
+    };
+}
+
+#[derive(Clone, Show, PartialEq, Copy)]
+pub enum Event {
+    ButtonDown(Button),
+    ButtonUp(Button),
+    Move(f64, f64),
+    Scroll(f64, f64),
+    Cadance(f64)
+}
+
+#[derive(Clone, Show, PartialEq, Copy)]
+pub enum WindowEvent {
+    MouseOver(bool),
+    Position(i32, i32),
+    Size(u32, u32)
+}
+
+#[derive(Clone, Show, PartialEq, Copy)]
+pub enum EventGroup {
+    Game(Event),
+    Window(WindowEvent),
+    Nop
+}
+
+impl Event {
+    pub fn from_glfw(evt: glfw::WindowEvent) -> EventGroup {
+        match evt {
+            glfw::WindowEvent::MouseButton(button, glfw::Action::Press, _) => {
+                EventGroup::Game(Event::ButtonDown(from_glfw_mouse_button(button)))
+            }
+            glfw::WindowEvent::MouseButton(button, glfw::Action::Release, _) => {
+                EventGroup::Game(Event::ButtonUp(from_glfw_mouse_button(button)))
+            }
+            glfw::WindowEvent::Key(button, _, glfw::Action::Press, _) => {
+                EventGroup::Game(Event::ButtonDown(from_glfw_key(button)))
+            }
+            glfw::WindowEvent::Key(button, _, glfw::Action::Release, _) => {
+                EventGroup::Game(Event::ButtonUp(from_glfw_key(button)))
+            }
+            glfw::WindowEvent::CursorPos(x, y) => {
+                EventGroup::Game(Event::Move(x, y))
+            }
+            glfw::WindowEvent::Scroll(x, y) => {
+                EventGroup::Game(Event::Scroll(x, y))
+            }
+            glfw::WindowEvent::CursorEnter(x) => {
+                EventGroup::Window(WindowEvent::MouseOver(x))
+            }
+            glfw::WindowEvent::Pos(x, y) => {
+                EventGroup::Window(WindowEvent::Position(x as i32, y as i32))
+            }
+            glfw::WindowEvent::FramebufferSize(x, y) => {
+                EventGroup::Window(WindowEvent::Size(x as u32, y as u32))
+            }
+            x => {
+                println!("unhandled {:?}", x);
+                EventGroup::Nop
+            }
+        }
+    }
+}
---
+++ src/snowmew-input/lib.rs
@@ -0,0 +1,440 @@
+//   Copyright 2014 Colin Sherratt
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+extern crate time;
+extern crate glfw;
+extern crate "rustc-serialize" as rustc_serialize;
+extern crate nice_glfw;
+extern crate ovr;
+extern crate collect;
+extern crate libc;
+
+use std::sync::Arc;
+use std::marker::Sync;
+use std::sync::mpsc::Receiver;
+#[cfg(target_os="linux")]
+use libc::c_void;
+
+use glfw::{WindowEvent, Glfw, Context, RenderContext};
+use glfw::WindowMode::{Windowed, FullScreen};
+use collect::TrieMap;
+
+mod input;
+
+pub type WindowId = usize;
+
+struct WindowHandle {
+    window: glfw::Window,
+    forced_event: Option<WindowEvent>,
+    receiver: Receiver<(f64, WindowEvent)>,
+    title: String
+}
+
+pub struct IOManager {
+    glfw: Glfw,
+    ovr: Option<ovr::Ovr>,
+    windows: TrieMap<WindowHandle>,
+    window_id: usize
+}
+
+fn create_window_context(glfw :&mut Glfw, width: u32, height: u32, name: &str, mode: glfw::WindowMode)
+        -> Option<(glfw::Window, Receiver<(f64,WindowEvent)>)> {
+
+    nice_glfw::WindowBuilder::new(glfw)
+        .try_modern_context_hints()
+        .size(width, height)
+        .title(name)
+        .mode(mode)
+        .create()
+}
+
+impl IOManager {
+    pub fn new(glfw: glfw::Glfw) -> IOManager {
+        IOManager {
+            glfw: glfw,
+            ovr: None,
+            windows: TrieMap::new(),
+            window_id: 0
+        }
+    }
+
+    fn add_window(&mut self, window: glfw::Window, recv: Receiver<(f64, WindowEvent)>) -> InputHandle {
+        let id = self.window_id;
+        self.window_id += 1;
+
+        let (w, h) = window.get_framebuffer_size();
+
+        self.windows.insert(id, {
+            WindowHandle {
+                window: window,
+                forced_event: Some(glfw::WindowEvent::FramebufferSize(w, h)),
+                receiver: recv,
+                title: "snowmew".to_string()
+            }
+        });
+
+        InputHandle{ handle: id }
+    }
+
+    pub fn window(&mut self, size: (u32, u32)) -> Option<Window> {
+        let (width, height) = size;
+        let win_opt = create_window_context(&mut self.glfw, width, height, "Snowmew", Windowed);
+        let (mut window, events) = match win_opt {
+            Some((window, events)) => (window, events),
+            None => return None
+        };
+        self.glfw.set_swap_interval(1);
+
+        window.set_all_polling(true);
+        window.show();
+        let version = window.get_context_version();
+        let rc = window.render_context();
+        let handle = self.add_window(window, events);
+
+        Some(Window {
+            handle: handle,
+            render: rc,
+            version: (version.major, version.minor),
+            hmd: None,
+            os_spec: WindowOSSpec::new(&self.glfw)
+        })
+    }
+
+    pub fn primary(&mut self, size: (u32, u32)) -> Option<Window> {
+        let screen = {
+            self.glfw.with_primary_monitor(|glfw, display| {
+                let display = display.unwrap();
+                let (width, height) = size;
+                create_window_context(glfw, width, height, "Snowmew FullScreen", FullScreen(display))
+            })
+        };
+
+        match screen {
+            None => None,
+            Some((mut window, events)) => {
+                window.set_all_polling(true);
+                window.show();
+                let version = window.get_context_version();
+                let rc = window.render_context();
+                let handle = self.add_window(window, events);
+
+                Some(Window {
+                    handle: handle,
+                    render: rc,
+                    version: (version.major, version.minor),
+                    hmd: None,
+                    os_spec: WindowOSSpec::new(&self.glfw)
+                })
+            }
+        }
+
+    }
+
+    pub fn get_primary_resolution(&mut self) -> (u32, u32) {
+        self.glfw.with_primary_monitor(|_, display| {
+            let display = display.expect("Could not get primnay display");
+            let vm = display.get_video_mode().expect("Could not get video mode");
+            (vm.width, vm.height)
+        })
+    }
+
+    pub fn get_primary_position(&mut self) -> (i32, i32) {
+        self.glfw.with_primary_monitor(|_, display| {
+            let display = display.expect("Could not get primnay display");
+            display.get_pos()
+        })
+    }
+
+    #[cfg(target_os="linux")]
+    fn create_hmd_window(&self, hmd: &ovr::HmdDescription) -> Option<(glfw::Window, Receiver<(f64,WindowEvent)>)> {
+        self.glfw.with_connected_monitors(|monitors| {
+            for m in monitors.iter() {
+                let (x, y) = m.get_pos();
+                if x == hmd.window_position.x && 
+                   y == hmd.window_position.y {
+                    let (width, height) = (hmd.resolution.x, hmd.resolution.y);
+                    let win_opt = self.create_window_context(width as u32, height as u32, "Snowmew FullScreen", FullScreen(m));
+                    let (window, events) = match win_opt {
+                        Some((window, events)) => (window, events),
+                        None => return None
+                    };
+
+                    return Some((window, events));
+                }
+            }
+
+            // fallback if we could not guess at the screen
+            let (width, height) = (hmd.resolution.x, hmd.resolution.y);
+            let win_opt = self.glfw.create_window(width as u32, height as u32, "Snowmew", Windowed);
+            let (window, events) = match win_opt {
+                Some((window, events)) => (window, events),
+                None => return None
+            };
+
+            // move viewport
+            let (dx, dy) = (hmd.window_position.x, hmd.window_position.y);
+            window.set_pos(dx as i32, dy as i32);
+
+            Some((window, events))
+
+        })
+    }
+
+    #[cfg(target_os="macos")]
+    fn create_hmd_window(&mut self, hmd: &ovr::HmdDescription) -> Option<(glfw::Window, Receiver<(f64,WindowEvent)>)> {
+        self.glfw.with_connected_monitors(|glfw, monitors| {
+            for m in monitors.iter() {
+                if !m.get_name()[].contains("Rift") {
+                    continue;
+                }
+
+                let (width, height) = (hmd.resolution.x, hmd.resolution.y);
+                let win_opt = create_window_context(glfw, width as u32, height as u32, "Snowmew FullScreen", FullScreen(m));
+                let (window, events) = match win_opt {
+                    Some((window, events)) => (window, events),
+                    None => return None
+                };
+
+                return Some((window, events));
+            }
+            None
+        })
+    }
+
+    pub fn hmd(&mut self) -> Option<Window> {
+        if !self.setup_ovr() {
+            return None;
+        }
+
+        let (window, events, rc, hmd) = {
+            let hmd = match self.ovr.as_ref().unwrap().first_hmd() {
+                Some(hmd) => hmd,
+                None => return None
+            };
+            let hmdinfo = hmd.get_description();
+
+            let (mut window, events) = match self.create_hmd_window(&hmdinfo) {
+                Some((window, events)) => (window, events),
+                None => return None
+            };
+
+            window.set_all_polling(true);
+            window.show();
+
+            let rc = window.render_context();
+            (window, events, rc, hmd)
+        };
+
+        let version = window.get_context_version();
+        let handle = self.add_window(window, events);
+
+        Some(Window {
+            handle: handle,
+            render: rc,
+            version: (version.major, version.minor),
+            hmd: Some(Arc::new(hmd)),
+            os_spec: WindowOSSpec::new(&self.glfw)
+        })
+    }
+
+    pub fn wait(&mut self) {
+        self.glfw.wait_events();
+    }
+
+    pub fn poll(&mut self) {
+        self.glfw.poll_events();
+    }
+
+    pub fn next_event(&mut self, handle: &InputHandle) -> input::EventGroup {
+        let evt = self.windows.get_mut(&handle.handle)
+        .map(|rx| {
+            // this is a hack to inject the correct size into the event buffer
+            match rx.forced_event.take() {
+                Some(evt) => return input::Event::from_glfw(evt),
+                None => ()
+            };
+            for (_, evt) in glfw::flush_messages(&rx.receiver) {
+                let evt = input::Event::from_glfw(evt);
+                if evt != input::EventGroup::Nop {
+                    return evt;
+                }
+            }
+            input::EventGroup::Nop
+        });
+
+        match evt {
+            Some(e) => e,
+            _ => input::EventGroup::Nop
+        }
+    }
+
+    pub fn should_close(&mut self, handle: &InputHandle) -> bool {
+        let should_close = self.windows.get_mut(&handle.handle)
+            .map(|win| win.window.should_close());
+
+        if let Some(x) = should_close {
+            x
+        } else {
+            true
+        }
+    }
+
+    pub fn set_title(&mut self, handle: &InputHandle, title: String) {
+        self.windows.get_mut(&handle.handle)
+            .map(|win| {
+                if title != win.title {
+                    win.window.set_title(&title[]);
+                    win.title = title.clone();
+                }
+            });
+    }
+
+    fn setup_ovr(&mut self) -> bool {
+        if self.ovr.is_some() &&
+           self.ovr.as_ref().unwrap().detect() > 0 {
+            return true;
+        }
+
+        if self.ovr.is_none() {
+            self.ovr = ovr::Ovr::init();
+        }
+
+        self.ovr.is_some() && self.ovr.as_ref().unwrap().detect() > 0
+    }
+
+    pub fn set_window_position(&mut self, window: &Window, pos: (i32, i32)) {
+        let (w, h) = pos;
+        match self.windows.get_mut(&window.handle.handle) {
+            Some(win) => win.window.set_pos(w, h),
+            None => ()
+        }
+    }
+
+    pub fn get_framebuffer_size(&mut self, window: &Window) -> (i32, i32) {
+        match self.windows.get_mut(&window.handle.handle) {
+            Some(win) => win.window.get_framebuffer_size(),
+            None => (0, 0)
+        }
+    }
+
+    pub fn get_proc_address(&self, name: &str) -> *const ::libc::c_void {
+        self.glfw.get_proc_address_raw(name)
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct InputHandle {
+    handle: usize,
+}
+
+#[cfg(target_os="macos")]
+struct WindowOSSpec;
+
+#[cfg(target_os="macos")]
+impl WindowOSSpec {
+    fn new(_: &Glfw) -> WindowOSSpec {WindowOSSpec}
+}
+
+#[cfg(target_os="linux")]
+struct WindowOSSpec {
+    display: *mut c_void
+}
+
+#[cfg(target_os="linux")]
+impl WindowOSSpec {
+    fn new(glfw: &Glfw) -> WindowOSSpec {
+        WindowOSSpec {
+            display: glfw.get_x11_display()
+        }
+    }
+}
+
+unsafe impl Send for WindowOSSpec {}
+
+pub struct Window {
+    handle: InputHandle,
+    render: RenderContext,
+    version: (u64, u64),
+    hmd: Option<Arc<ovr::Hmd>>,
+    os_spec: WindowOSSpec
+}
+
+impl Window {
+    pub fn swap_buffers(&mut self) {
+        self.render.swap_buffers()
+    }
+
+    pub fn make_context_current(&mut self) {
+        self.render.make_current()
+    }
+
+    pub fn get_context_version(&self) -> (u64, u64) {
+        self.version
+    }
+
+    pub fn handle(&self) -> InputHandle {
+        self.handle.clone()
+    }
+
+    pub fn is_hmd(&self) -> bool {
+        self.hmd.is_some()
+    }
+
+    pub fn get_hmd<'a>(&'a self) -> Arc<ovr::Hmd> {
+        self.hmd.as_ref().expect("no hmd device found!").clone()
+    }
+
+    /// Wrapper for `glfwGetGLXContext`
+    #[cfg(target_os="linux")]
+    pub fn get_x11_display(&self) -> *mut c_void {
+        self.os_spec.display
+    }
+}
+
+#[derive(Clone, RustcEncodable, RustcDecodable)]
+pub struct IoState {
+    pub render_size: (u32, u32),
+    pub size: (u32, u32),
+    pub position: (i32, i32),
+    pub show_mouse: bool,
+    pub mouse_over: bool,
+    pub window_title: String
+}
+
+impl IoState {
+    pub fn new() -> IoState {
+        IoState {
+            render_size: (800, 600),
+            size: (800, 600),
+            position: (0, 0),
+            show_mouse: true,
+            mouse_over: false,
+            window_title: "snowmew".to_string()
+        }
+    }
+
+    pub fn window_action(&mut self, win: input::WindowEvent) {
+        match win {
+            input::WindowEvent::Size(x, y) => {
+                self.size = (x, y);
+            }
+            input::WindowEvent::Position(x, y) => {
+                self.position = (x, y);
+            }
+            input::WindowEvent::MouseOver(mouse) => {
+                self.mouse_over = mouse;
+            }
+        }
+    }
+}
