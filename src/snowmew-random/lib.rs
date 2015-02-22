74a86cdb245bbcbd00b1762e52877d9d7d277f2f
Colin Sherratt <colin.sherratt@gmail.com>
2015-02-22 17:14:20 -0500

    use crates.io rand

--- src/snowmew-random/lib.rs
+++ src/snowmew-random/lib.rs
@@ -12,11 +12,10 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-#![feature(rand)]
-
 extern crate "snowmew-core" as core;
+extern crate rand;
 
-use std::rand::{Rng, ChaChaRng};
+use rand::{Rng, ChaChaRng};
 
 #[derive(Copy)]
 pub struct RandomData {
